94ee4df153cffd6c6a40e14015531cdac0d21fb2
Serejenka <mikheevs11@gmail.com>
2019-06-22 19:36:04 +0300

    Added settings.ejs + some minor changes

--- routes/account.js
+++ routes/account.js
@@ -8,7 +8,11 @@ router.use((req, res, next) => {
     next();
 });
 router.get('/settings', checkIsLogged, (req, res, next) => {
-    res.render('settings', {user: req.user});
+    res.render('settings', {user: req.body.user});
+});
+
+router.post('/settings', (req, res, next) => {
+    res.send('hello');
 });
 
 module.exports = router;
