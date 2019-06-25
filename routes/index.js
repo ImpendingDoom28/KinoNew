1b14ebda80a7b404601f58a22a43e497fb0f6af0
Rinat Shigapov <r89179060010@gmail.com>
2019-06-25 11:35:08 +0300

    movies

--- routes/index.js
+++ routes/index.js
@@ -11,28 +11,51 @@ router.get('/home', checkIsLogged, (req, res) => {
     //TODO Ринат, вставлять сюда
     var request = require("request");
 
-    var options = { method: 'GET',
+    var options = {
+        method: 'GET',
         url: 'https://api.themoviedb.org/3/discover/movie',
         qs:
-            { with_genres: '28',
+            {
+                with_genres: '28',
                 'vote_average.gte': '8',
                 page: '1',
                 include_video: 'false',
                 include_adult: 'false',
                 sort_by: 'popularity.desc',
                 language: 'ru-Ru',
-                api_key: 'f1bb885a34819055db8514823f6050a4' },
-        body: '{}' };
+                api_key: 'f1bb885a34819055db8514823f6050a4'
+            },
+        body: '{}'
+    };
 
     request(options, function (error, response, body) {
         if (error) throw new Error(error);
 
-        console.log(body);
+        // console.log(body.page);
+        let elem = JSON.parse(body);
+        let results = elem.results;
+        let movies = [];
+        console.log(results);
+        for (let i = 0; i < results.length; i++) {
+            movies[i] = {
+                id: results[i].id,
+                title: results[i].title,
+                genre: results[i].genre_ids,
+                poster: results[i].poster_path,
+                release_date: results[i].release_date,
+                vote_average: results[i].vote_average,
+                overview: results[i].overview
+            }
+        }
+        movies.forEach(function(elem) {
+            console.log(elem);
+        });
     });
 
     res.render('home', {
         user: req.user
     });
+
 });
 
 module.exports = router;
