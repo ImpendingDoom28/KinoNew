d949473c206fa906b6538873e7ad12d5e35043e6
Serejenka <mikheevs11@gmail.com>
2019-06-25 13:09:33 +0300

    Removed tmdbv3 dependency

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const {checkIsLogged, checkIsNotLogged} = require('../config/auth');
-//TODO Ринат, расскоменти и если ошибки - реши
-//const tmdb = require('tmdbv3').init(require('../config/keys').tmdbKey);
 
 // Main page is the login page
 router.get('/', checkIsNotLogged, (req, res) => res.redirect('/users/login'));
