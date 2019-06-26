2728fffb20028f01aa699cbe79b6840edc504c35
Serejenka <mikheevs11@gmail.com>
2019-06-26 09:35:18 +0300

    Added sync-request dependency (i feel depressed)

--- routes/account.js
+++ routes/account.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 const { checkIsLogged } = require('../config/auth');
-
+const request = require("sync-request");
 const User = require('../models/User');
 
 router.get('/settings', checkIsLogged, (req, res, next) => {
@@ -19,49 +18,68 @@ function isUnique(toCheck, array) {
 }
 
 function isGenre(arrayToCheck) {
-    console.log("array: " + '\'' + arrayToCheck +'\'');
     if(arrayToCheck !== '') {
-        var request = require("request");
-        var options = {
-            method: 'GET',
-            url: 'https://api.themoviedb.org/3/genre/movie/list',
+        const options = {
             qs:
                 {
                     language: 'ru-Ru',
                     api_key: 'f1bb885a34819055db8514823f6050a4'
-                },
-            body: '{}'
+                }
         };
-        request(options, function (error, response, body) {
-            if (error) throw new Error(error);
-            const genres = JSON.parse(body);
-            arrayToCheck.forEach((item) => {
-                let help = false;
-                genres.genres.forEach(((genre) => {
-                    if(item === genre.name) {
-                        help = true;
-                    }
-                }));
-                if(!help) {
-                    return help;
+        const req = request('GET', 'https://api.themoviedb.org/3/genre/movie/list', options);
+        const genres = JSON.parse(req.getBody());
+        arrayToCheck.forEach((item) => {
+            let help = false;
+            genres.genres.forEach((genre) => {
+                if(item === genre.name) {
+                    help = true;
                 }
             });
+            if(!help) {
+                return help;
+            }
         });
         return true;
     } else {
         return true;
     }
-
+}
+function getGenre(genre) {
+    const options = {
+        qs:
+            {
+                language: 'ru-Ru',
+                api_key: 'f1bb885a34819055db8514823f6050a4'
+            }
+    };
+    const id = [];
+    const req = request('GET', 'https://api.themoviedb.org/3/genre/movie/list', options);
+    const genres = JSON.parse(req.getBody());
+    genres.genres.forEach((actualGenre) => {
+        if (genre === actualGenre.name) {
+            console.log('id from request: ' + actualGenre.id);
+            id.push(actualGenre.id);
+        }
+    });
+    return id;
 }
 
 router.post('/settings', checkIsLogged, (req, res, next) => {
     const {favGenre, favActors, countries, minRating} = req.body;
     const errors = [];
+    console.log(favGenre);
     if(favGenre.length === 0 && favActors.length === 0) {
         errors.push({msg: 'Вы ничего не указали!'});
     } else {
         if(!isGenre(favGenre)) {
-            errors.push({msg: 'Вы указали не допустимый жанр!'})
+            errors.push({msg: 'Вы указали не допустимый жанр!'});
+        }
+    }
+    if(typeof minRating !== Number && minRating !== '') {
+        errors.push({msg: 'Минимальный рейтинг должен быть числом!'});
+    } else {
+        if((minRating < 1 || minRating > 10) && minRating !== '') {
+            errors.push({msg: 'Минимальный рейтинг должен быть в пределах от 1 до 10!'});
         }
     }
     if(errors.length > 0) {
@@ -73,7 +91,9 @@ router.post('/settings', checkIsLogged, (req, res, next) => {
                     if(favGenre.length > 0) {
                         favGenre.forEach((item) => {
                             if(item !== '' && isUnique(item, user.favGenres)) {
+                                const id = getGenre(item);
                                 user.favGenres.push(item);
+                                user.favGenresIDs.push(id[0]);
                             }
                         });
                     }
@@ -123,7 +143,9 @@ router.post('/settings/reset', (req, res, next) => {
             if(user) {
                 console.log('Нашли пользователя');
                 user.favGenres = [];
+                user.favGenresIDs = [];
                 user.favActors = [];
+                user.favActorsIDs = [];
                 user.countries = [];
                 user.minRating = '';
                 res.render('settings', {
